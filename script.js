8f979472af16492ee87a04245f4a16b4ef7266c8
suchit20 <96559769+suchit20@users.noreply.github.com>
2023-10-25 15:54:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let profilemenu = document.getElementById("profilemenu");
+function toggleMenu(){
+    profilemenu.classList.toggle("open-menu");
+   
+
+}
+let sideActivity = document.getElementById("sidebarActivity");
+let morelink = document.getElementById("showMoreLink");
+
+  function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+  }
